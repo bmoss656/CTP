70e99da3eed593ea0f7b155dfb80963bf1ce9b2d
Brad Moss <bradley2.moss@live.uwe.ac.uk>
2018-12-17 13:03:27 +0000

    Shop 3D viewing

--- AppProject/Assets/Scripts/Shop/ItemControl.cs
+++ AppProject/Assets/Scripts/Shop/ItemControl.cs
@@ -2,15 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemControl : MonoBehaviour {
+public class ItemControl : MonoBehaviour
+{
+    public List<GameObject> items;
+    private GameObject currentObj;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private int objectNum = 0;
+    private int maxNum = 0;
+    private int shopNum = 0;
+
+    
+    private void OnDisable()
+    {
+        this.gameObject.SetActive(false);
+        Destroy(currentObj);
+    }
+
+
+    public void ActivateItems(int i)
+    {
+        maxNum = items[i].transform.childCount - 1;
+        shopNum = i;
+        this.gameObject.SetActive(true);
+        currentObj = Instantiate(items[shopNum]);
+    }
+
+    public void NextObject()
+    {
+        if(objectNum == maxNum)
+        {
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(false);
+            objectNum = 0;
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(true);
+        }
+        else
+        {
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(false);
+            objectNum += 1;
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(true);
+        }
+       
+    }
+
+    public void PreviousObject()
+    {
+        if (objectNum == 0)
+        {
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(false);
+            objectNum = maxNum;
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(true);
+        }
+        else
+        {
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(false);
+            objectNum -= 1;
+            currentObj.transform.GetChild(objectNum).gameObject.SetActive(true);
+        }
+    }
+
 }
---
+++ AppProject/Assets/Scripts/Shop/RotateItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItem : MonoBehaviour
+{
+
+    public float speed = 20f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+}

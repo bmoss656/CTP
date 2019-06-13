7d0af82db5b31ba40d75fb4f85c46f0ad9b117ae
Brad Moss <bradley2.moss@live.uwe.ac.uk>
2019-06-13 15:59:07 +0100

    Visual Improvements
    -Should have done this on general branch
    -Updated tasks, menu, shop and house scenes to use new ui
    -Made new main menu

---
+++ AppProject/Assets/CameraOrbit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public GameObject orbit;
+    public float rotateSpeed = 200;
+
+    void Update ()
+    {
+        transform.RotateAround(orbit.transform.position, orbit.transform.up, rotateSpeed * Time.deltaTime);
+    }
+}

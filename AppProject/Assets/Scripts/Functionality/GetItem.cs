b99b53850a1b0b4a19ddfaafc5918e5c1f861296
Brad Moss <bradley2.moss@live.uwe.ac.uk>
2019-06-18 12:54:03 +0100

    Sprites load in for placing

--- AppProject/Assets/Scripts/Functionality/GetItem.cs
+++ AppProject/Assets/Scripts/Functionality/GetItem.cs
@@ -50,12 +50,17 @@ public class GetItem : MonoBehaviour
         if (itemName == "NULL")
         {
             transform.GetChild(0).gameObject.SetActive(false);
+            
             isEmpty = true;
         }
         else
         {
             //Set image to something
             transform.GetChild(0).gameObject.SetActive(true);
+            if(Resources.Load("Sprites/" + itemName))
+            {
+                transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load("Sprites/" + itemName, typeof(Sprite)) as Sprite;
+            }
             isEmpty = false;
         }
     }
@@ -64,87 +69,90 @@ public class GetItem : MonoBehaviour
 
     void Update()
     {
-        if (!isEmpty)
+        if (buildCanvas)
         {
-            //Check if the left Mouse button is clicked
-            if (Application.platform == RuntimePlatform.WindowsEditor)
+            if (!isEmpty)
             {
-                if (Input.GetMouseButtonDown(0))
+                //Check if the left Mouse button is clicked
+                if (Application.platform == RuntimePlatform.WindowsEditor)
                 {
-                    m_PointerEventData = new PointerEventData(m_EventSystem);
-                    m_PointerEventData.position = Input.mousePosition;
-                    List<RaycastResult> results = new List<RaycastResult>();
-                    m_Raycaster.Raycast(m_PointerEventData, results);
-
-                    foreach (RaycastResult result in results)
+                    if (Input.GetMouseButtonDown(0))
                     {
-                        Debug.Log(result.gameObject.name);
-                        if (result.gameObject.name == gameObject.name)
+                        m_PointerEventData = new PointerEventData(m_EventSystem);
+                        m_PointerEventData.position = Input.mousePosition;
+                        List<RaycastResult> results = new List<RaycastResult>();
+                        m_Raycaster.Raycast(m_PointerEventData, results);
+
+                        foreach (RaycastResult result in results)
                         {
-                            if (isSelected)
+                            Debug.Log(result.gameObject.name);
+                            if (result.gameObject.name == gameObject.name)
                             {
-                                isSelected = false;
-                            }
-                            else
-                            {
-                                isSelected = true;
+                                if (isSelected)
+                                {
+                                    isSelected = false;
+                                }
+                                else
+                                {
+                                    isSelected = true;
+                                }
                             }
                         }
                     }
-                }
-                else if (Input.GetMouseButtonUp(0))
-                {
-                    RayToSpawnPos();
-                    isSelected = false;
-                    transform.GetChild(0).position = startingPos;
-                    InventoryObject.SetActive(true);
-                    GetComponent<Image>().enabled = true;
-                    transform.parent = lastParent;
-                    transform.GetComponentInParent<ResetInventory>().ResetInv();
-                }
-            }
-            else if (Application.platform == RuntimePlatform.Android)
-            {
-                if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-                {
-                    m_PointerEventData = new PointerEventData(m_EventSystem);
-                    m_PointerEventData.position = Input.GetTouch(0).position;
-                    List<RaycastResult> results = new List<RaycastResult>();
-                    m_Raycaster.Raycast(m_PointerEventData, results);
-
-                    foreach (RaycastResult result in results)
+                    else if (Input.GetMouseButtonUp(0))
                     {
-                        Debug.Log(result.gameObject.name);
-                        if (result.gameObject.name == gameObject.name)
+                        RayToSpawnPos();
+                        isSelected = false;
+                        transform.GetChild(0).position = startingPos;
+                        InventoryObject.SetActive(true);
+                        GetComponent<Image>().enabled = true;
+                        transform.parent = lastParent;
+                        transform.GetComponentInParent<ResetInventory>().ResetInv();
+                    }
+                }
+                else if (Application.platform == RuntimePlatform.Android)
+                {
+                    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+                    {
+                        m_PointerEventData = new PointerEventData(m_EventSystem);
+                        m_PointerEventData.position = Input.GetTouch(0).position;
+                        List<RaycastResult> results = new List<RaycastResult>();
+                        m_Raycaster.Raycast(m_PointerEventData, results);
+
+                        foreach (RaycastResult result in results)
                         {
-                            if (isSelected)
+                            Debug.Log(result.gameObject.name);
+                            if (result.gameObject.name == gameObject.name)
                             {
-                                isSelected = false;
-                            }
-                            else
-                            {
-                                isSelected = true;
+                                if (isSelected)
+                                {
+                                    isSelected = false;
+                                }
+                                else
+                                {
+                                    isSelected = true;
+                                }
                             }
                         }
                     }
+                    else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                    {
+                        RayToSpawnPos();
+                        isSelected = false;
+                        transform.GetChild(0).position = startingPos;
+                        InventoryObject.SetActive(true);
+                        GetComponent<Image>().enabled = true;
+                        transform.parent = lastParent;
+                        transform.GetComponentInParent<ResetInventory>().ResetInv();
+                    }
                 }
-                else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+                if (isSelected)
                 {
-                    RayToSpawnPos();
-                    isSelected = false;
-                    transform.GetChild(0).position = startingPos;
-                    InventoryObject.SetActive(true);
-                    GetComponent<Image>().enabled = true;
-                    transform.parent = lastParent;
-                    transform.GetComponentInParent<ResetInventory>().ResetInv();
+                    transform.parent = buildCanvas.transform;
+                    GetComponent<Image>().enabled = false;
+                    InventoryObject.SetActive(false);
+                    transform.GetChild(0).position = Input.mousePosition;
                 }
-            }
-            if (isSelected)
-            {
-                transform.parent = buildCanvas.transform;
-                GetComponent<Image>().enabled = false;
-                InventoryObject.SetActive(false);
-                transform.GetChild(0).position = Input.mousePosition;
             }
         }
     }

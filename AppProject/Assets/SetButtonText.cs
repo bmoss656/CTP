2d0aa0c5c184801e04af3bb645d1eb5dfcb1e49f
Brad Moss <bradley2.moss@live.uwe.ac.uk>
2019-07-01 16:44:07 +0100

    Started Adding Choosable Tasks

---
+++ AppProject/Assets/SetButtonText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SetButtonText : MonoBehaviour
+{
+    private int position;
+
+
+    public void SetText(string[] text)
+    {
+        for(int i =0; i < text.Length;i++)
+        {
+            Debug.Log(text[i]);
+        }
+        int max = position * 5;
+        int z = 0;
+        Debug.Log(max);
+        for(int i = max; i < max + 5; i++)
+        {
+            Debug.Log(i);
+            if (!string.IsNullOrEmpty(text[i]))
+            {
+                transform.GetChild(z).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = text[i];
+            }
+            else
+            {
+                transform.GetChild(z).gameObject.SetActive(false);
+            }
+            z++;
+        }
+    }
+
+
+	public void SetPos(int num)
+    {
+        position = num;
+    }
+}
---
+++ AppProject/Assets/SwitchPage.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPage : MonoBehaviour
+{
+    private List<GameObject> buttons;
+    private int maxObjects;
+    private int currentObj = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        buttons = new List<GameObject>();
+	}
+	
+	public void AddButtons(GameObject obj)
+    {
+        buttons.Add(obj);
+        maxObjects = buttons.Count;
+    }
+
+    public void NextObject()
+    {
+        if (maxObjects == 1 || buttons.Count == 0)
+        {
+            return;
+        }
+        else
+        {
+            buttons[currentObj].SetActive(false);
+            if((currentObj + 1) >= maxObjects)
+            {
+                currentObj = 0;
+            }
+            else
+            {
+                currentObj++;
+            }
+            buttons[currentObj].SetActive(true);
+        }
+    }
+
+    public void PreviousObject()
+    {
+        if (maxObjects == 1 || buttons.Count == 0)
+        {
+            return;
+        }
+        else
+        {
+            buttons[currentObj].SetActive(false);
+            if ((currentObj - 1) < 0)
+            {
+                currentObj = maxObjects - 1;
+            }
+            else
+            {
+                currentObj--;
+            }
+            buttons[currentObj].SetActive(true);
+        }
+    }
+}
